b9213f27c5f322e89238e3fc094eaf2416afead6
juandavidas <juandavidas@be9c2166-0c52-4c4f-1ddb-ab3e542e0dc3>
2011-11-23 16:04:29 +0000

    Este es el proyecto. Hasta el momento verifica los datos.

---
+++ AnalizadorLR/src/analizadorlr/pila.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package analizadorlr;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author juanda
+ */
+public class pila {
+    
+   // public String crearpila()
+            Stack pila = new Stack();
+
+}
